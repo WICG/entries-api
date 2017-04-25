da0fad135abe07fa2e9ebcfb660f04cb38618bc7
Joshua Bell <jsbell@chromium.org>
2017-04-25 15:00:47 -0700

    Add interfaces-manual; simplify manual tests

--- tests/support.js
+++ tests/support.js
@@ -5,37 +5,44 @@
 setup({explicit_timeout: true});
 
 const tests = [];
-const elem = document.getElementById('drop');
-elem.addEventListener('dragover', e => {
-  e.preventDefault();
-});
-elem.addEventListener('drop', e => {
-  e.preventDefault();
-  const entry = e.dataTransfer.items[0].webkitGetAsEntry();
-  window.entry = entry;
-  elem.parentElement.removeChild(elem);
+window.addEventListener('DOMContentLoaded', e => {
+  const header = document.createElement('h1');
+  header.innerText = document.title;
+  document.body.appendChild(header);
+  const elem = document.createElement('div');
+  elem.style = 'height: 50px; border: 1px dotted red;';
+  elem.innerHTML = 'Drop the <b>support/upload</b> directory here.</div>';
+  document.body.appendChild(elem);
+  elem.addEventListener('dragover', e => {
+    e.preventDefault();
+  });
+  elem.addEventListener('drop', e => {
+    e.preventDefault();
+    const item = e.dataTransfer.items[0];
+    const entry = item.webkitGetAsEntry();
+    window.entry = entry;
+    elem.parentElement.removeChild(elem);
 
-  tests.forEach(f => f(entry));
+    tests.forEach(f => f(entry, item));
+  });
 });
 
 
 // Registers a test to be run when an entry is dropped. Calls |func|
-// with (test, entry); |func| and must call `test.done()` when complete.
+// with (test, entry, item); |func| must call `test.done()` when complete.
 function entry_test(func, description) {
   const test = async_test(description);
-  tests.push(test.step_func(arg => func(test, arg)));
-}
-function run_tests(arg) {
+  tests.push(test.step_func((entry, item) => func(test, entry, item)));
 }
 
 // Registers a test to be run when an entry is dropped. Digs the named
 // |file| out of the dropped entry and calls |func| with
 // (test, file_entry); |func| must call `test.done()` when complete.
 function file_entry_test(name, func, description) {
-  return entry_test((t, entry) => {
+  return entry_test((t, entry, item) => {
     entry.getFile(
       name, {},
-      t.step_func(entry => func(t, entry)),
+      t.step_func((entry) => func(t, entry)),
       t.unreached_func('getFile should not fail'));
   }, description);
 }
